4aea96217933bbbb119dc890cef31acef7292fbe
rajeshwari1101 <rajeshwaripatil1101@gmail.com>
2022-03-06 22:20:25 +0530

    Adding validation for first name

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========Welcome To User Registration===========");
+            //Creating object od class
+            UserRegistration registration = new UserRegistration();
+            registration.FirstName();
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ UserRegistration/UserRegistration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    internal class UserRegistration
+    {
+        //Creating method to check validation for first name
+        public void FirstName()
+        {
+            Console.Write("Enter the name = ");
+            string name = Console.ReadLine();
+            string Pattern = "^[A-Z][a-zA-Z]{2,}$";
+
+            if (Validate(name, Pattern))
+                Console.WriteLine("The entered name is valid!!");
+            else
+                Console.WriteLine("Please enter the valid name..");
+        }
+
+        //Creating method to check validation
+        public bool Validate(string info, string Pattern)
+        {
+            return Regex.IsMatch(info, Pattern);
+        }
+
+    }
+}
